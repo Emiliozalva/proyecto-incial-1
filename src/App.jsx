d409346438b48c8739c57a6c84697737e2dbbf62
Emilio Zalva <emizalva08@gmail.com>
2025-11-25 11:32:59 -0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import viteLogo from '/vite.svg'
 import Navbar from './componentes/Navbar/Navbar'
 import ItemListContainer from './componentes/ItemListContainer/ItemListContainer';
 import './App.css'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <> 
+    <BrowserRouter> { /* Recorda que esta envuelto en app en wrap de borwser router */ }
     <Navbar />
+    <Routes>
+      { /* Aqui van las rutas */ }
+    </Routes>
     <ItemListContainer saludoT="Bienvenido a mi sitio de comercio electronico." />
+    </BrowserRouter>
     </>
   )
 }
